e283f31ea3afa701eb971d4b94d479c65654ecef
Thozamile Mbalo <tmbalo02@gmail.com>
2021-07-13 20:11:40 +0200

    top 100 selects

--- VersionSQL/DESKTOP-6GM0SJ6/SQL2017/mbalotechDev/dbo/Procedure/usp_get_results.sql
+++ VersionSQL/DESKTOP-6GM0SJ6/SQL2017/mbalotechDev/dbo/Procedure/usp_get_results.sql
@@ -1,5 +1,5 @@
 /****** Object:  Procedure [dbo].[usp_get_results]    Committed by VersionSQL https://www.versionsql.com ******/
 
-CREATE proc usp_get_results
+CREATE proc [dbo].[usp_get_results]
 as
-select top 10 * from dbo.snp500_update
+select top 100 * from dbo.snp500_update
