989d322d2626e2888401d2bc3906b530118713dd
Thozamile <tmbalo02@gmail.com>
2021-07-13 21:15:04 +0200

    latested update

--- Select.sql
+++
@@ -1,5 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (10) [Date]
-      ,[Time]
-      ,[Range_In_pips]
-  FROM [mbalotech].[dbo].[snp500_update]
--- VersionSQL/DESKTOP-6GM0SJ6/SQL2017/mbalotechDev/dbo/Procedure/usp_get_results.sql
+++
@@ -1,8 +0,0 @@
-/****** Object:  Procedure [dbo].[usp_get_results]    Committed by VersionSQL https://www.versionsql.com ******/
-
-CREATE proc [dbo].[usp_get_results]
-as
-select 
-	top 1000 * 
-from 
-	dbo.snp500_update
